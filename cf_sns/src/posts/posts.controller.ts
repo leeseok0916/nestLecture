b8cbfa3c31aed6dc56812971a9cc0235f1871017
twostones <revelation0072@gmail.com>
2024-06-13 17:07:28 +0900

    service 로직 추가

--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -1,41 +1,13 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-interface Post {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: Post[] = [
-  {
-    id: 1,
-    author: '홍길동',
-    title: '제목 1',
-    content: '내용 1',
-    likeCount: 10,
-    commentCount: 5,
-  },
-  {
-    id: 2,
-    author: '이순신',
-    title: '제목 2',
-    content: '내용 2',
-    likeCount: 20,
-    commentCount: 12,
-  },
-  {
-    id: 3,
-    author: '김구',
-    title: '제목 3',
-    content: '내용 3',
-    likeCount: 30,
-    commentCount: 35,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -43,20 +15,14 @@ export class PostsController {
 
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   @Get(':id')
   // @Get(':id/author/:authorId')
   // getPost(@Param('id') id: string, @Param('authorId') authorId: string) {
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException('게시글을 찾을 수 없습니다.');
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   @Post()
@@ -65,16 +31,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post = {
-      id: posts.length + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-    posts.push(post);
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   @Put(':id')
@@ -84,45 +41,12 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException('게시글을 찾을 수 없습니다.');
-    }
-
-    if (author) {
-      post.author = author;
-    }
-    if (title) {
-      post.title = title;
-    }
-    if (content) {
-      post.content = content;
-    }
-
-    // posts = posts.map((p) => (p.id === +id ? post : p));
-
-    // const updatedPost = {
-    //   ...posts[index],
-    //   author,
-    //   title,
-    //   content,
-    // };
-
-    // posts[index] = updatedPost;
-
-    return post;
+    console.log('content:', content);
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException('게시글을 찾을 수 없습니다.');
-    }
-
-    posts = posts.filter((post) => post.id !== +id);
-    return id;
+    return this.postsService.deletePost(+id);
   }
 }
--- cf_sns/src/posts/posts.service.ts
+++ cf_sns/src/posts/posts.service.ts
@@ -1,4 +1,108 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+export interface Post {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: Post[] = [
+  {
+    id: 1,
+    author: '홍길동',
+    title: '제목 1',
+    content: '내용 1',
+    likeCount: 10,
+    commentCount: 5,
+  },
+  {
+    id: 2,
+    author: '이순신',
+    title: '제목 2',
+    content: '내용 2',
+    likeCount: 20,
+    commentCount: 12,
+  },
+  {
+    id: 3,
+    author: '김구',
+    title: '제목 3',
+    content: '내용 3',
+    likeCount: 30,
+    commentCount: 35,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException('게시글을 찾을 수 없습니다.');
+    }
+
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post = {
+      id: posts.length + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts.push(post);
+    return post;
+  }
+
+  updatePost(id: number, author: string, title: string, content: string) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException('게시글을 찾을 수 없습니다.');
+    }
+
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+
+    // posts = posts.map((p) => (p.id === +id ? post : p));
+
+    // const updatedPost = {
+    //   ...posts[index],
+    //   author,
+    //   title,
+    //   content,
+    // };
+
+    // posts[index] = updatedPost;
+
+    return post;
+  }
+
+  deletePost(id: number) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException('게시글을 찾을 수 없습니다.');
+    }
+
+    posts = posts.filter((post) => post.id !== +id);
+    return id;
+  }
+}
